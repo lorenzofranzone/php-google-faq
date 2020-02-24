a6963b706f2a3bc301cbe953788f834bdb2852fe
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-24 16:42:52 +0100

    Get Answer (non funziona)

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
 
   // Code
-  
+  $db = [
+
+    'faq1' => [
+      question => 'Question 1',
+      answer => 'Lorem ipsum dolor sit amet 1',
+      answer => 'Lorem ipsum dolor sit amet 1',
+      answer => 'Lorem ipsum dolor sit amet 1',
+      answer => 'Lorem ipsum dolor sit amet 1',
+      answer => 'Lorem ipsum dolor sit amet 1'
+    ],
+
+    'faq2' => [
+      question => 'Question 2',
+      answer => 'Lorem ipsum dolor sit amet 2'
+    ],
+
+    'faq3' => [
+      question => 'Question 3',
+      answer => 'Lorem ipsum dolor sit amet 3'
+    ],
+
+    'faq4' => [
+      question => 'Question 4',
+      answer => 'Lorem ipsum dolor sit amet 4'
+    ],
+
+  ];
 
 ?>
 
@@ -15,7 +41,21 @@
 </head>
 <body>
 
+  <div class="container faq">
+    <!-- Prendi dall'array $db tutte le chiavi "faq-n" -->
+    <?php foreach($db as $key => $value) { ?>
 
+      <!-- QUESTIONS -->
+      <h3><?php echo $key ?></h3>
+
+      <!-- ANSWERS -->
+      <!-- Prendi da ogni "faq-n" tutti i valori delle chiavi "answer" -->
+      <?php foreach($value as $key => $answer) { ?>
+        <p><?php echo $answer['answer'] ?></p>
+      <?php } ?>
+
+    <?php } ?>
+  </div>
 
 </body>
 </html>
