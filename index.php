4ac491af2bacd20f1eda3d9f1dc719946b35de92
Lorenzo <lorenzo.franzone@gmail.com>
2020-02-24 16:59:26 +0100

    error

--- index.php
+++ index.php
@@ -4,27 +4,27 @@
   $db = [
 
     'faq1' => [
-      question => 'Question 1',
-      answer => 'Lorem ipsum dolor sit amet 1',
-      answer => 'Lorem ipsum dolor sit amet 1',
-      answer => 'Lorem ipsum dolor sit amet 1',
-      answer => 'Lorem ipsum dolor sit amet 1',
-      answer => 'Lorem ipsum dolor sit amet 1'
+      'question' => 'Question 1',
+      'answer' => 'Lorem ipsum dolor sit amet 1',
+      'answer' => 'Lorem ipsum dolor sit amet 1',
+      'answer' => 'Lorem ipsum dolor sit amet 1',
+      'answer' => 'Lorem ipsum dolor sit amet 1',
+      'answer' => 'Lorem ipsum dolor sit amet 1'
     ],
 
     'faq2' => [
-      question => 'Question 2',
-      answer => 'Lorem ipsum dolor sit amet 2'
+      'question' => 'Question 2',
+      'answer' => 'Lorem ipsum dolor sit amet 2'
     ],
 
     'faq3' => [
-      question => 'Question 3',
-      answer => 'Lorem ipsum dolor sit amet 3'
+      'question' => 'Question 3',
+      'answer' => 'Lorem ipsum dolor sit amet 3'
     ],
 
     'faq4' => [
-      question => 'Question 4',
-      answer => 'Lorem ipsum dolor sit amet 4'
+      'question' => 'Question 4',
+      'answer' => 'Lorem ipsum dolor sit amet 4'
     ],
 
   ];
@@ -44,16 +44,13 @@
   <div class="container faq">
     <!-- Prendi dall'array $db tutte le chiavi "faq-n" -->
     <?php foreach($db as $key => $value) { ?>
-
       <!-- QUESTIONS -->
       <h3><?php echo $key ?></h3>
-
       <!-- ANSWERS -->
       <!-- Prendi da ogni "faq-n" tutti i valori delle chiavi "answer" -->
       <?php foreach($value as $key => $answer) { ?>
         <p><?php echo $answer['answer'] ?></p>
       <?php } ?>
-
     <?php } ?>
   </div>
 
